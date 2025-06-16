2dac9a855b895b7fb239e720916eef6a75d46572
Marlon-Sbardelatti <hetzwga@gmail.com>
2025-06-16 09:06:52 -0300

    get profile

--- routes/routes.go
+++ routes/routes.go
@@ -16,6 +16,7 @@ func RegisterRoutes(r chi.Router, app *app.App) {
 		r.Post("/{id}/dislike/{post-id}", handlers.DislikePostRequest(app))
 		r.Post("/login", handlers.LoginHandler(app))
 		r.Get("/", handlers.GetAllUsersHandler(app))
+		r.Get("/profile/{id}", handlers.GetProfileHandler(app))
 		r.Get("/{id}", handlers.GetUserByIdHandler(app))
 		r.Get("/{id}/followers", handlers.GetFollowersRequest(app))
 		r.Get("/{id}/following", handlers.GetFollowingRequest(app))
